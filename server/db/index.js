76bfed0ae9cda38a53e7940d27c3fbee430511b8
Pushpender Singh <pushpendersingh673@gmail.com>
2020-05-12 17:50:19 +0530

    Dockerfile

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,41 @@
 const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+dotenv.config();
+
+// mongoose options
+const options = {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  autoIndex: false,
+  poolSize: 10,
+  bufferMaxEntries: 0
+};
+
+// mongodb environment variables
+const {
+    MONGO_HOSTNAME,
+    MONGO_DB,
+    MONGO_PORT
+} = process.env;
+
+const dbConnectionURL = {
+    'LOCALURL': `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`
+};
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/cinema', { useNewUrlParser: true })
+    .connect(dbConnectionURL.LOCALURL, options)
     .catch(e => {
         console.error('Connection error', e.message)
     })
 
 const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'Mongodb Connection Error:' + dbConnectionURL.LOCALURL));
+db.once('open', () => {
+     // we're connected !
+     console.log('Mongodb Connection Successful');
+});
 
 module.exports = db
